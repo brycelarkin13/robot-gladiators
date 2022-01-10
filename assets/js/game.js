ace1f48ca9f42ea3e1f072b5fa2c21eb7b90b5af
bryce <blarkin13@gmail.com>
2022-01-10 17:20:06 -0600

    random health and attck values

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,14 +33,17 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -59,7 +62,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -92,14 +97,14 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
-    // if player is till alive and we're not at last enemy in array 
+    // if player is still alive and we're not at last enemy in array 
     if (playerHealth > 0 && i < enemyNames.length - 1) {
       // ask play if they want to use store before next round
       var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
@@ -142,6 +147,7 @@ else {
 }
 };
 
+// Shop function
 var shop = function() {
   // ask player what they would like to do
   var shopOptionPrompt = window.prompt(
@@ -191,6 +197,13 @@ var shop = function() {
     break;
 
   }
+};
+
+// function to generate random numeric values
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 // start game when page loads
